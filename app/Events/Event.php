bf75d0f4478604f505416468cb6e46ecb1492712
Adam Tester <adam@genyx.co.uk>
2015-09-18 10:52:52 +0100

    Laravel Init

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TargetInk\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TargetInk\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
