50b148f16dd48da94ebaa26c3682cbaff41bfa69
Eugenio Gomez <eugenio.gomez@heliocentrix.co.uk>
2015-09-23 15:16:59 +0100

    Base Menu

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/', 'AppController@index');
+//That route can be removed if redirect if auth or register is changed to /
+Route::get('/home', 'AppController@index');
+
+Route::resource('/maintenance', 'MaintenanceController');
+Route::resource('/seo', 'SEOController');
+Route::resource('/docs', 'DocumentsController');
