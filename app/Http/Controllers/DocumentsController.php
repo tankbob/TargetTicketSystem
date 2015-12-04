069d4cab0d1524facc0d6085a4e09a6aafc1b504
Eugenio Gomez <eugenio.gomez@heliocentrix.co.uk>
2015-12-04 10:33:30 +0000

    Add middleware

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -13,6 +13,7 @@ class DocumentsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('ownCompany');
     }
 
     /**
