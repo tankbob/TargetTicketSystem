36cc122839c5d809594c0126fe20e50498305759
Adam Tester <adam@genyx.co.uk>
2015-12-07 13:32:08 +0000

    Title

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title') @if(isset($title)) {{ $title }} @else Target Ink @endif @show</title>
+        <title>@yield('sectionTitle') | Target Ink</title>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300|Montserrat" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
