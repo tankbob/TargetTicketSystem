bed94d3e3b644efcb07f5e39b820aa25dcfe973b
Adam Tester <adam@genyx.co.uk>
2015-12-17 08:54:00 +0000

    Better seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use TargetInk\User;
 use TargetInk\Ticket;
 use TargetInk\Advert;
+use TargetInk\Response;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,36 +18,101 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        $faker = Faker\Factory::create();
 
-        $letters = range('a', 'j');
-        foreach ($letters as $l) {
-            User::create([
-                'name' => $l.$l.$l.' '.$l.$l.$l,
-                'email' => $l.'@'.$l.'.'.$l,
-                'company' => $l.$l.'Centrix',
-                'company_slug' => $l.$l.'centrix',
-                'web' => 'www.'.$l.".co.uk",
-                'admin' => ($l == 'b'),
-                'password' => bcrypt('123456')
+        // Create admin users
+        User::create([
+            'name' => 'Sample User',
+            'email' => 'dev@heliocentrix.co.uk',
+            'company' => 'Heliocentrix',
+            'company_slug' => 'heliocentrix',
+            'web' => 'http://www.heliocentrix.co.uk',
+            'admin' => true,
+            'password' => bcrypt('secret')
+        ]);
+
+        User::create([
+            'name' => 'Heliocentrix',
+            'email' => 'user@heliocentrix.co.uk',
+            'company' => 'Heliocentrix',
+            'company_slug' => 'heliocentrix',
+            'web' => 'http://www.heliocentrix.co.uk',
+            'admin' => false,
+            'password' => bcrypt('secret')
+        ]);
+
+        User::create([
+            'name' => 'Rob Stevens',
+            'email' => 'rob@targetink.co.uk',
+            'company' => 'Target Ink',
+            'company_slug' => 'target_ink',
+            'web' => 'http://www.targetink.co.uk',
+            'admin' => true,
+            'password' => bcrypt('secret')
+        ]);
+
+        User::create([
+            'name' => 'Sample User',
+            'email' => 'user@targetink.co.uk',
+            'company' => 'Target Ink',
+            'company_slug' => 'target_ink',
+            'web' => 'http://www.targetink.co.uk',
+            'admin' => false,
+            'password' => bcrypt('secret')
+        ]);
+
+        $usersToSeed = 25;
+        for ($i = 1; $i <= $usersToSeed; $i++) {
+            $company = $faker->company;
+            $company_slug = strtolower(str_replace(' ', '_', $company));
+            $company_slug = str_replace(',', '', $company_slug);
+            $company_slug = str_replace('.', '', $company_slug);
+
+            $user = User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'company' => $company,
+                'company_slug' => $company_slug,
+                'web' => $faker->domainName,
+                'admin' => false,
+                'password' => bcrypt('secret')
             ]);
-            if($l != 'b'){
-                for($i = 1; $i < rand(1, 6); $i++){
-                    Ticket::create([
-                        'title' => 'Ticket '.$i,
-                        'client_id' => User::where('email', $l.'@'.$l.'.'.$l)->first()->id
+
+            // Create tickets
+            for($t = 1; $t < rand(1, 6); $t++){
+                $ticket = Ticket::create([
+                    'title' => $faker->text(80),
+                    'client_id' => $user->id,
+                ]);
+
+                // Create some responses
+                $resAdmin = true;
+                for($r = 1; $r < rand(2, 10); $r++) {
+                    $response = Response::create([
+                        'ticket_id' => $ticket->id,
+                        'working_time' => rand(0, 100),
+                        'admin' => $resAdmin,
+                        'content' => $faker->paragraph
                     ]);
-                }
-                for($i = 1; $i < rand(1, 6); $i++){
-                    Advert::create([
-                        'name' => 'Advert '.$i,
-                        'client_id' => User::where('email', $l.'@'.$l.'.'.$l)->first()->id,
-                        'url' => 'http://www.google.es'
-                    ]);
+
+                    if($resAdmin) {
+                        $resAdmin = false;
+                    } else {
+                        $resAdmin = true;
+                    }
                 }
             }
-        }
 
-        // $this->call(UserTableSeeder::class);
+            // Create some adverts
+            for($a = 1; $a < rand(1, 6); $a++){
+                Advert::create([
+                    'name' => $faker->text(80),
+                    'client_id' => $user->id,
+                    'url' => 'http://www.google.com'
+                ]);
+            }
+
+        }
 
         Model::reguard();
     }
