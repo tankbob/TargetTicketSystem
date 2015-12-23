a5ca50db9a5e160437f08c4e93e6f65e208ddf23
Adam Tester <adam@genyx.co.uk>
2015-12-23 11:27:54 +0000

    TIM-22 nozoom lock on mobile

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <title>@yield('sectionTitle') | Target Ink</title>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300|Montserrat" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
