b7a395e5898794b5f26b122bacbd8095a93160f6
Eugenio Gomez <eugenio.gomez@heliocentrix.co.uk>
2016-01-04 10:42:14 +0000

    Token missmatch erros taken out

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -18,6 +19,7 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         HttpException::class,
         ModelNotFoundException::class,
+        TokenMismatchException::class,
     ];
 
     /**
@@ -44,6 +46,9 @@ class Handler extends ExceptionHandler
     {
         if ($e instanceof ModelNotFoundException) {
             $e = new NotFoundHttpException($e->getMessage(), $e);
+        }
+        if ($e instanceof TokenMismatchException){
+            return \Redirect::back()->withErrors(['session' => 'Sorry, the session has expired. Please try it again.']);
         }
 
         return parent::render($request, $e);
