2b9cf32771ebcd170c79fb88d2bd40cb5c969685
Adam Tester <adam@genyx.co.uk>
2016-01-11 10:21:01 +0000

    TIM-49 Mobile layout needs more/consistent padding

--- resources/views/dashboard/adverts/advertList.blade.php
+++ resources/views/dashboard/adverts/advertList.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-4 clearfix">
         <h2 class="maintenance-title pull-left">Adverts</h2> <a href="#" class="bannerFormToggler icon-new-advert pull-left" clientId="0"></a>
     </div>
-    <div class="col-sm-6 text-right client-chooser">
-        <label for="banner-customer-select" class="hidden-xs">Choose a Client:</label>
-        {!! Form::select('client_id', [ '' => 'Choose a Client...' ] + $clients, request()->input('client_id'), ['id' => 'banner-customer-select']) !!}
+    <div class="col-sm-8 clearfix text-right client-chooser">
+        <label for="banner-customer-select" class="hidden-xs hidden-sm">Choose a Client:</label>
+        {!! Form::select('client_id', [ '' => 'Choose a Client...' ] + $clients, request()->input('client_id'), ['id' => 'banner-customer-select', 'class' => 'form-control']) !!}
     </div>
 </div>
